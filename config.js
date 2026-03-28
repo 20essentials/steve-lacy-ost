d29e2e8b609487497691fd55644464e4ac0e49ea
DominicDev <s0l0estudi4r@gmail.com>
2026-03-28 14:33:35 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'STEVE LACY';
+export let nameSongs = [
+	"DARK RED", /*1*/
+	"BAD HABIT", /*2*/
+	"MERCURY", /*3*/
+	"GIVE YOU THE WORLD", /*4*/
+	"SUNSHINE", /*5*/
+	"HELMET", /*6*/
+	"BUTTONS", /*7*/
+	"AMBER", /*8*/
+	"PLAYGROUND", /*9*/
+	"N SIDE", /*10*/
+	"LAY ME DOWN", /*11*/
+	"LOVE 2 FAST", /*12*/
+	"LIKE ME", /*13*/
+	"OUTRO FREESTYLE / 4EVER", /*14*/
+	"BASEMENT JACK", /*15*/
+	"CODY FREESTYLE", /*16*/
+	"GIVE YOU THE WORLD", /*17*/
+	"THATS NO FUN", /*18*/
+	"OUT OF ME HEAD", /*19*/
+	"4REAL", /*20*/
+]
